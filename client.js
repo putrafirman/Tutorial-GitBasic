33281c79d1011f3bef1fac93a853f359b20b29c0
Putra Firman Ardiansyah <ardidroid97@gmail.com>
2017-01-19 10:54:36 +0700

    add client.js

---
+++ client.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {Router, Route, IndexRoute, hashHistory} from "react-router";
+
+import Layout2 from "./pages/Layout2";
+import Detail from "./pages/Detail";
+import Home from "./pages/Home";
+import Aboutus from "./pages/Aboutus";
+import ProductList from "./pages/ProductList";
+
+
+var app = document.getElementById('app');
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Layout2}>
+      <IndexRoute component={Home}></IndexRoute>
+      <Route path="productlist" component={ProductList}></Route>
+      <Route path="about" component={Aboutus}></Route>
+      <Route path="detail/:id" component={Detail}></Route>
+    </Route>
+  </Router>
+  , app);
